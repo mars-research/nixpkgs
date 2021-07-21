0fd2b598f1e3dffca7960cfe28dfb2b92077ec2b
Sylvain Bellemare <sbellem@gmail.com>
2021-07-21 00:42:32 -0400

    Address some of the PR comments

--- pkgs/os-specific/linux/ippcrypto/default.nix
+++ pkgs/os-specific/linux/ippcrypto/default.nix
@@ -1,5 +1,6 @@
 { lib,
   stdenvNoCC,
+  fetchpatch,
   fetchurl,
   fetchFromGitHub,
   autoconf,
@@ -27,12 +28,24 @@ stdenvNoCC.mkDerivation {
   pname = "ippcrypto";
   version = "ippcp_2020u3";
   src = fetchFromGitHub {
-    owner = "sbellem";
+    owner = "intel";
     repo = "linux-sgx";
-    rev = "e6036d6edb4371f2acc64c50b7cb51e9dfa439a4";
-    sha256 = "0znallianv3lp3y62cfdgp8gacpw516qg8cjxhz8bj5lv5qghchk";
+    rev = "0cea078f17a24fb807e706409972d77f7a958db9";
+    sha256 = "1cr2mkk459s270ng0yddgcryi0zc3dfmg9rmdrdh9mhy2mc1kx0g";
     fetchSubmodules = true;
   };
+  patches = [
+    (fetchpatch {
+      name = "replace-bin-cp-with-cp.patch";
+      url = "https://github.com/intel/linux-sgx/pull/730.patch";
+      sha256 = "0xwlpm1r4rl4anfhjkr6fgz0gcyhr0ng46fv8iw9hfsh891yqb7z";
+    })
+    (fetchpatch {
+      name = "ipp-crypto-makefile.patch";
+      url = "https://github.com/intel/linux-sgx/pull/731.patch";
+      sha256 = "1q9rsygm92kiwdj81yxp9q182rgb19kxir2m2r9l73hxwfz1cc0a";
+    })
+  ];
   buildInputs = [
     binutils
     autoconf
@@ -57,8 +70,8 @@ stdenvNoCC.mkDerivation {
   dontConfigure = true;
   # sgx expects binutils to be under /usr/local/bin by default
   preBuild = ''
-    export BINUTILS_DIR=$binutils/bin
-    '';
+    export BINUTILS_DIR=${binutils}/bin
+  '';
   buildPhase = ''
     runHook preBuild
 
@@ -69,15 +82,14 @@ stdenvNoCC.mkDerivation {
     make MITIGATION-CVE-2020-0551=CF
 
     runHook postBuild
-    '';
-  postBuild = ''
+  '';
+  installPhase = ''
     mkdir -p $out
     cp -r ./lib $out/lib
     cp -r ./inc $out/inc
     cp -r ./license $out/license
     ls -l ./inc/
   '';
-  dontInstall = true;
   dontFixup = true;
 
   meta = with lib; {
--- pkgs/os-specific/linux/sgxsdk/default.nix
+++ pkgs/os-specific/linux/sgxsdk/default.nix
@@ -1,5 +1,6 @@
 { lib,
   stdenvNoCC,
+  fetchpatch,
   fetchurl,
   fetchFromGitHub,
   autoconf,
@@ -20,17 +21,17 @@
   openssl,
   perl,
   python3,
-  texinfo
+  texinfo,
 }:
 
 stdenvNoCC.mkDerivation {
-  pname = "sgxsdk";
-  version = "2.13.3a0";
+  pname = "sgx-sdk";
+  version = "2.14";
   src = fetchFromGitHub {
-    owner = "sbellem";
+    owner = "intel";
     repo = "linux-sgx";
-    rev = "e6036d6edb4371f2acc64c50b7cb51e9dfa439a4";
-    sha256 = "0znallianv3lp3y62cfdgp8gacpw516qg8cjxhz8bj5lv5qghchk";
+    rev = "0cea078f17a24fb807e706409972d77f7a958db9";
+    sha256 = "1cr2mkk459s270ng0yddgcryi0zc3dfmg9rmdrdh9mhy2mc1kx0g";
     fetchSubmodules = true;
   };
   dontConfigure = true;
@@ -38,7 +39,19 @@ stdenvNoCC.mkDerivation {
     patchShebangs ./linux/installer/bin/build-installpkg.sh
     patchShebangs ./linux/installer/common/sdk/createTarball.sh
     patchShebangs ./linux/installer/common/sdk/install.sh
-    '';
+  '';
+  patches = [
+    (fetchpatch {
+      name = "replace-bin-cp-with-cp.patch";
+      url = "https://github.com/intel/linux-sgx/pull/730.patch";
+      sha256 = "0xwlpm1r4rl4anfhjkr6fgz0gcyhr0ng46fv8iw9hfsh891yqb7z";
+    })
+    (fetchpatch {
+      name = "ipp-crypto-makefile.patch";
+      url = "https://github.com/intel/linux-sgx/pull/731.patch";
+      sha256 = "1q9rsygm92kiwdj81yxp9q182rgb19kxir2m2r9l73hxwfz1cc0a";
+    })
+  ];
   buildInputs = [
     autoconf
     automake
@@ -61,8 +74,8 @@ stdenvNoCC.mkDerivation {
     nasm
   ];
   preBuild = ''
-    export BINUTILS_DIR=$binutils/bin
-    '';
+    export BINUTILS_DIR=${binutils}/bin
+  '';
   buildPhase = ''
     runHook preBuild
 
@@ -77,13 +90,13 @@ stdenvNoCC.mkDerivation {
     make sdk_install_pkg
 
     runHook postBuild
-    '';
+  '';
   postBuild = ''
     patchShebangs ./linux/installer/bin/sgx_linux_x64_sdk_*.bin
-    '';
+  '';
   installPhase = ''
     echo -e 'no\n'$out | ./linux/installer/bin/sgx_linux_x64_sdk_*.bin
-    '';
+  '';
   dontFixup = true;
 
   meta = with lib; {
